c37cc0a058181135f44ace14b7e888ebcbc3ad01
Douglas Bernardo <jkdouglas21@gmail.com>
2022-01-29 13:36:11 -0300

    Add stripe and faunaDb integration

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -8,11 +12,45 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
-          params: {
-              scope: 'read:user',
-          }
+        params: {
+          scope: 'read:user',
+        }
       }
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+      console.log(email);
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch (error) {
+        console.log(error)
+        return false;
+      }
+    },
+  }
 })
